9599705a4d29161327593a78bb4123a6c1639cea
Charlie <charliewangchenyu@gmail.com>
2016-06-21 14:37:15 +0000

    minor change

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 
 var express=require("express");
 var app=express();
+var port=process.env.PORT||8080;
 
 
 app.get('/*',function(req,res){
   res.end("hello");
+});
+
+app.listen(port,function(){
+  console.log("the app is listening at port: "+port);
 });
